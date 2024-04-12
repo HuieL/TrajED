fcdd751df45c0277867db9bb34900c04ba7ce677
HuieL <82495316+HuieL@users.noreply.github.com>
2024-04-12 04:22:25 -0400

    Update Transfomer_utils.py

--- Transfomer_utils.py
+++ Transfomer_utils.py
@@ -2,7 +2,6 @@ import math
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-import ipdb
 
 # Ensure that all operations are deterministic on GPU (if used) for reproducibility
 torch.backends.cudnn.deterministic = True
